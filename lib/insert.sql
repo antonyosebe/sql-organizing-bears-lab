4008965299b5c6cf513b2b91567a9af071f1cf8f
antonyosebe <osebe.anto@gmail.com>
2023-02-17 17:16:14 +0300

    commit tree 17ec6cb5416cc15a4bfd99c228547f80c480c335

---
+++ lib/insert.sql
@@ -0,0 +1,11 @@
+INSERT INTO bears
+    (name,age,sex,color,temperament,alive)
+VALUES
+    ("MR. Chocolate", 4, "F", "Black", "Sanguine", 1),
+    ("Rowdy", 7, "M", "Brown", "Melancholic", 0),
+    ("Tabitha", 3, "F", "White", "Choleric", 1),
+    ("Sergeant Brown", 10, "M", "Brown", "Phlegmatic", 1),
+    ("Melissa", 9, "F", "Grey", "Choleric", 0),
+    ("Grinch", 5, "M", "Black White", "Sanguine", 1),
+    ("Wendy", 2 , "F", "White", "Melancholic", 1),
+    (NULL, 1, "M", "Blue", "Phlegmaticc", 1);
